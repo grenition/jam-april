db6928f6cc8d254d435ac7514a7e253b5a8ad354
Arssseniy <88106737+grenition@users.noreply.github.com>
2024-04-27 00:09:53 +0300

    feat: top down camera

---
+++ Assets/Scripts/Player/TopDownCamera/TopDownFollower.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownFollower : MonoBehaviour
+{
+    [SerializeField] private float _followSpeed = 5f;
+    [SerializeField] private float _distance = 10f;
+    [SerializeField] private Transform _target;
+
+    private Vector3 _position;
+    private Quaternion _rotation;
+    private Vector3 _startPosition;
+
+    private void OnEnable()
+    {
+        _position = transform.position;
+        _rotation = transform.rotation;
+        _startPosition = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 horizontal = Vector3.Lerp(_position, _target.position, _followSpeed * Time.deltaTime);
+        horizontal = VectorMathf.RemoveDotVector(horizontal, Vector3.up);
+
+        Vector3 vertical = VectorMathf.ExtractDotVector(_startPosition, Vector3.up) + Vector3.up * _distance;
+
+        _position = horizontal + vertical;
+
+        transform.position = _position;
+        transform.rotation = _rotation;
+    }
+}
