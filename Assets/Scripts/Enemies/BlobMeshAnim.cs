4640df887f33e87d70912878f8ae5e06b960fdba
VogBog <vogbog92@gmail.com>
2024-04-28 15:01:19 +0300

    Destroy blobs anim

--- Assets/Scripts/Enemies/BlobMeshAnim.cs
+++ Assets/Scripts/Enemies/BlobMeshAnim.cs
@@ -1,5 +1,5 @@
+using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BlobMeshAnim : MonoBehaviour
@@ -24,5 +24,20 @@ public class BlobMeshAnim : MonoBehaviour
             _renderer.SetBlendShapeWeight(i, 40 * Mathf.Sin(_keys[i]) + 50);
             _keys[i] += Time.deltaTime * _speed;
         }
+    }
+
+    public void DestroyBlob()
+    {
+        StartCoroutine(DestroyAnimIE());
+    }
+
+    private IEnumerator DestroyAnimIE()
+    {
+        var scale = transform.localScale;
+        transform.DOScale(scale * 1.2f, 1);
+        yield return new WaitForSeconds(1);
+        transform.DOScale(Vector3.zero, 1);
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
